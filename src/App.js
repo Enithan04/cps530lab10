596f4eea48691e33a4ebb7faf75cc2d9a69f8ef6
Enithan04 <enithan1@gmail.com>
2024-12-04 19:22:33 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ fontFamily: "Arial, sans-serif", padding: "20px", textAlign: "center" }}>
+      <h1>Problem 3: Simple React Web Page</h1>
+      
+      <h3>How the Page Was Built</h3>
+      <p> Firstly I went into my text editior(VSCODE) and installed Node.js which allows me to access the npm commands.
+        Next I used the command npx create-react-app cps530lab10 which set up the structure of the react app. Then finally I open 
+        the folder I created and edit the App.js file to change the frontend to include the 
+        content I wanted. 
+      </p>
+
+      <h3>Difficulties Encountered</h3>
+      <p> Since I have created a react app before there wasn't to many difficulties creating it.
+        It was pretty simple installing everything and adding the content needed. I did have difficulties 
+        trying the find a way to make this accessible for everyone especially for the TA to be able to mark. 
+        The solution to this was I did my own research and found out I can do this thorugh github. Finding out 
+        how exactly I can host it I was able to find the correct commands and push it onto my github.
+      </p>
     </div>
   );
 }
 
 export default App;
+
